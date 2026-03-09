a9747dfc0d9e9e34123dc4b075406e35a322ea61
Kascho <mail@inorange.org>
2026-03-09 23:01:04 +0100

    Redesign news index grid

--- app/controllers/public/news_controller.rb
+++ app/controllers/public/news_controller.rb
@@ -4,7 +4,9 @@ module Public
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
     def index
+      @news_query = params[:q].to_s.strip
       @blog_posts = BlogPost.published_live.with_attached_cover_image
+      @blog_posts = apply_query(@blog_posts, @news_query) if @news_query.present?
     end
 
     def show
@@ -12,6 +14,15 @@ module Public
     end
 
     private
+      def apply_query(relation, query)
+        token = "%#{ActiveRecord::Base.sanitize_sql_like(query)}%"
+
+        relation.where(
+          "blog_posts.title ILIKE :token OR blog_posts.teaser ILIKE :token",
+          token: token
+        )
+      end
+
       def render_not_found
         render plain: "Nicht gefunden", status: :not_found
       end
