90175e0dc4b990361de046fdfbd9c91a4eb8ff5f
Kascho <mail@inorange.org>
2026-03-10 08:15:05 +0100

    Add newsletter signup to news index

--- app/controllers/public/newsletter_subscribers_controller.rb
+++ app/controllers/public/newsletter_subscribers_controller.rb
@@ -3,13 +3,13 @@ module Public
     allow_unauthenticated_access only: [ :create ]
 
     def create
-      subscriber = NewsletterSubscriber.new(newsletter_subscriber_params.merge(source: "homepage"))
+      subscriber = NewsletterSubscriber.new(newsletter_subscriber_params.merge(source: newsletter_source))
 
       if subscriber.save
-        redirect_to events_path(filter: params[:filter].presence, view: params[:view].presence, q: params[:q].presence),
+        redirect_to newsletter_redirect_target,
                     notice: "Danke. Du bist jetzt fuer den Newsletter eingetragen."
       else
-        redirect_to events_path(filter: params[:filter].presence, view: params[:view].presence, q: params[:q].presence),
+        redirect_to newsletter_redirect_target,
                     alert: subscriber.errors.full_messages.to_sentence
       end
     end
@@ -18,6 +18,14 @@ module Public
 
     def newsletter_subscriber_params
       params.require(:newsletter_subscriber).permit(:email)
+    end
+
+    def newsletter_redirect_target
+      params[:return_to].presence || events_path(filter: params[:filter].presence, view: params[:view].presence, q: params[:q].presence)
+    end
+
+    def newsletter_source
+      params[:source].presence || "homepage"
     end
   end
 end
