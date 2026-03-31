3600aec116902b021b912bee5da4abd1b817c9d1
Marc <marc@schopper.rocks>
2026-04-01 00:53:40 +0200

    feat(sentry): integrate Sentry for error tracking and performance monitoring

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
+gem "sentry-ruby"
+gem "sentry-rails"
+
 group :development, :test do
   gem "dotenv-rails"
   gem "bullet"
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+end
