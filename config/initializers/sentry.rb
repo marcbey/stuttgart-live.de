cfdee92be10ef393174975c3c20ea2e32be1d8f5
Marc Schopper <marc@schopper.rocks>
2026-04-01 08:49:52 +0200

    feat(sentry): add stackprof gem and configure logging settings

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,8 +3,14 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-  config.enable_logs = true
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
+
+  # Logging
+  config.enable_logs = true
+
+  # stackprof
+  config.traces_sample_rate = 1.0
+  config.profiles_sample_rate = 1.0
 end
