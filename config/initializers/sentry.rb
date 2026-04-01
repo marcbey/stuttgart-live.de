c14cc3c73ac9a76bd900dd6abdd84452c61f5e22
Marc Schopper <marc@schopper.rocks>
2026-04-01 12:32:26 +0200

    fix(sentry): disable SDK in test and development

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,27 +3,32 @@
 # config/initializers/sentry.rb
 require "active_support/parameter_filter"
 
-Sentry.init do |config|
-  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+sentry_environment = ENV.fetch("SENTRY_ENVIRONMENT", Rails.env)
+disabled_sentry_environments = %w[test development].freeze
 
-  config.enabled_environments = %w[production]
-  config.environment = ENV.fetch("SENTRY_ENVIRONMENT", Rails.env)
-  config.release = ENV["SENTRY_RELEASE"]
+unless disabled_sentry_environments.include?(sentry_environment)
+  Sentry.init do |config|
+    config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
 
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.enabled_environments = %w[production]
+    config.environment = sentry_environment
+    config.release = ENV["SENTRY_RELEASE"]
 
-  # Am Anfang lieber konservativ
-  config.send_default_pii = false
-  config.enable_logs = true
-  config.traces_sample_rate = 0.1
-  config.profiles_sample_rate = 0.0
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
-  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+    # Am Anfang lieber konservativ
+    config.send_default_pii = false
+    config.enable_logs = true
+    config.traces_sample_rate = 0.1
+    config.profiles_sample_rate = 0.0
 
-  config.before_send = lambda do |event, _hint|
-    event.extra    = filter.filter(event.extra)    if event.extra
-    event.user     = filter.filter(event.user)     if event.user
-    event.contexts = filter.filter(event.contexts) if event.contexts
-    event
+    filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+
+    config.before_send = lambda do |event, _hint|
+      event.extra    = filter.filter(event.extra)    if event.extra
+      event.user     = filter.filter(event.user)     if event.user
+      event.contexts = filter.filter(event.contexts) if event.contexts
+      event
+    end
   end
 end
