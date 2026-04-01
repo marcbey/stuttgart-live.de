0328cfc6a766c921fc34c933503f9d9191fe5d66
Marc Schopper <marc@schopper.rocks>
2026-04-01 08:47:46 +0200

    fix(sentry): enable logging in Sentry configuration

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-
+  config.enable_logs = true
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
