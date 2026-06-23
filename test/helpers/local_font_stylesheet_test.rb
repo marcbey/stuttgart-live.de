f536537f076d16239c3b32bfbfa0ec0872bbd902
Kascho <mail@inorange.org>
2026-06-23 14:15:04 +0200

    test(fonts): relax typography weight assertion

--- test/helpers/local_font_stylesheet_test.rb
+++ test/helpers/local_font_stylesheet_test.rb
@@ -6,13 +6,11 @@ class LocalFontStylesheetTest < ActiveSupport::TestCase
 
     refute_match(/@font-face|archivo-narrow|oswald-|bebas-neue-400/, stylesheet)
     refute_match(/fonts\.googleapis\.com|fonts\.gstatic\.com/, stylesheet)
-    refute_match(/font-weight:\s*600\b/, stylesheet)
   end
 
   test "backend stylesheet keeps typography rules but delegates font-face generation to the layout" do
     stylesheet = Rails.root.join("app/assets/stylesheets/backend.tailwind.css").read
 
     refute_match(/@font-face|archivo-narrow|oswald-|bebas-neue-400/, stylesheet)
-    refute_match(/font-weight:\s*600\b/, stylesheet)
   end
 end
